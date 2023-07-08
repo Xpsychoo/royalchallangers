f4a1ed45bc744ff34d6be51cafc7564e2a5efb7a
Xpsychoo <shivvbhakt43@gmail.com>
2023-07-09 01:04:08 +0530

    first commit

--- js/script.js
+++ js/script.js
@@ -8,3 +8,25 @@ function myFunction() {
         sidebar.style.display = "block";
     }
 }
+$(document).ready(function () {
+
+    $("#owl-example").owlCarousel({
+        loop: true,
+        margin: 5,
+        nav: false,
+        autoplay: true,
+        dots:true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            }
+        }
+    });
+
+});
